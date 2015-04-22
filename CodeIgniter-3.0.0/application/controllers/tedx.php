3efd1721fe07f5f2d9cc86839b046c917c5e6abc
Kenny Vanrusselt <Kenny_Vanrusselt@hotmail.be>
2015-04-22 17:46:19 +0200

    nav bar enhancements

--- CodeIgniter-3.0.0/application/controllers/tedx.php
+++ CodeIgniter-3.0.0/application/controllers/tedx.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Navigation extends CI_Controller {
+class tedx extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('Navigation');
+		$this->load->view('Navigation_View');
 	}
 }
---
+++ CodeIgniter-3.0.0/application/views/Navigation_View.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="csstransforms no-csstransforms3d csstransitions">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>TEDxUHasselt</title>
+		<link rel="stylesheet" type="text/css" href="/NothingToSeeHere/CodeIgniter-3.0.0/assets/css/Navigation/font-awesome.css">
+		<link rel="stylesheet" type="text/css" href="/NothingToSeeHere/CodeIgniter-3.0.0/assets/css/Navigation/menu.css">
+
+		<script type="text/javascript" src="/NothingToSeeHere/CodeIgniter-3.0.0/assets/js/Navigation/jquery.js"></script>
+		<script type="text/javascript" src="/NothingToSeeHere/CodeIgniter-3.0.0/assets/js/Navigation/function.js"></script>
+
+	</head>
+	<body>
+
+		<div id="wrap">
+			<header>
+				<div class="inner relative">
+					<a class="logo" href="http://www.freshdesignweb.com"><img src="/NothingToSeeHere/CodeIgniter-3.0.0/assets/imgs/Navigation/logo.png" alt="fresh design web"></a>
+					<a id="menu-toggle" class="button dark" href="#"><i class="icon-reorder"></i></a>
+					<nav id="navigation">
+						<ul id="main-menu">
+							<li class="current-menu-item">
+								<a href="http://www.freshdesignweb.com">Home</a>
+							</li>
+							<li class="parent">
+								<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">News</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html"><i class="icon-wrench"></i> Events</a>
+									</li>
+									<li>
+										<a class="parent" href="#"><i class="icon-file-alt"></i> Pages</a>
+										<ul class="sub-menu">
+											<li>
+												<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Full Width</a>
+											</li>
+											<li>
+												<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Left Sidebar</a>
+											</li>
+											<li>
+												<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Right Sidebar</a>
+											</li>
+											<li>
+												<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Double Sidebar</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Forums</a>
+							</li>
+							<li class="parent">
+								<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">About</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">About TEDxUHASSELT</a>
+									</li>
+									<li>
+										<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Our Team</a>
+									</li>
+									<li>
+										<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Alumni Members</a>
+									</li>
+									<li>
+										<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Our Partners</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="http://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol.html">Contact</a>
+							</li>
+						</ul>
+					</nav>
+					<div class="clear"></div>
+				</div>
+			</header>
+		</div>
+	</body>
+</html>
