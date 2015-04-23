c1f072c384b18dce2189125fe3676b1a6a9b4225
Jorissen9 <bjorn_jorissen@hotmail.com>
2015-04-23 21:41:38 +0200

    basic login

--- CodeIgniter-3.0.0/application/views/Userpanel_LoggedOut_View.php
+++ CodeIgniter-3.0.0/application/views/Userpanel_LoggedOut_View.php
@@ -2,7 +2,7 @@
     <div class="">
         <div class="col-xs-6 avatarhide">
             <div class="col-md-4 pull-right">
-             <a class="pull-right form-control btn" href="#login">Login</a>
+             <a class="pull-right form-control btn" href="<?php echo site_url('login/index') ?>">Login</a>
              <a class="pull-right form-control btn" href="">Register</a>
             </div>
         </div>
--- CodeIgniter-3.0.0/application/views/login_view.php
+++ CodeIgniter-3.0.0/application/views/login_view.php
@@ -1,19 +1,40 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
- <head>
-   <title>Simple Login with CodeIgniter</title>
- </head>
- <body>
-   <h1>Simple Login with CodeIgniter</h1>
-   <?php echo validation_errors(); ?>
-   <?php echo form_open('http://localhost:6969/NothingToSeeHere/CodeIgniter-3.0.0/index.php/verifylogin'); ?>
-     <label for="username">Username:</label>
-     <input type="text" size="20" id="username" name="username"/>
-     <br/>
-     <label for="password">Password:</label>
-     <input type="password" size="20" id="passowrd" name="password"/>
-     <br/>
-     <input type="submit" value="Login"/>
-   </form>
- </body>
+	<head>
+		<title>Simple Login with CodeIgniter</title>
+	</head>
+	<body>
+		<?php echo validation_errors(); ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-4 col-md-offset-4">
+					<h1 class="text-center login-title">Sign in to continue</h1>
+					<div class="account-wall">
+						<div class="col-md-12">
+							<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">
+						</div>
+						<div class="col-sm-12">
+							<form class="form-signin" action="<?php echo site_url('verifylogin/index') ?>">
+								<div class="col-sm-4">
+									<input type="text" class="form-control" placeholder="Email" required autofocus>
+								</div>
+								<div class="col-sm-4">
+									<input type="password" class="form-control" placeholder="Password" required>
+								</div>
+								<div class="col-sm-4">
+									<button class="btn btn-lg btn-primary btn-block" type="submit">
+										Sign in
+									</button>
+									<label class="checkbox pull-left">
+										<input type="checkbox" value="remember-me">
+										Remember me </label>
+								</div>
+							</form>
+						</div>
+					</div>
+					<a href="#" class="text-center new-account">Create an account </a>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
