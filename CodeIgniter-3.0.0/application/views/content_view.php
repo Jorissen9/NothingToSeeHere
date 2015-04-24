c0c80523298cf7517a8f60a407b1099755f1c471
Kenny Vanrusselt <Kenny_Vanrusselt@hotmail.be>
2015-04-24 14:34:04 +0200

    facebook newsfeed api

--- CodeIgniter-3.0.0/application/views/content_view.php
+++ CodeIgniter-3.0.0/application/views/content_view.php
@@ -7,7 +7,25 @@
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</p>
+
 		<br/>
 		<h1> Nieuws </h1>
+		<?php
+		$page_id = 'TEDxUHasselt';
+		$access_token = '384545391746432|kMtO9DQn412t0WTd6MfLo3fsKkI';
+		//Get the JSON
+		$json_object = @file_get_contents('https://graph.facebook.com/' . $page_id . '/posts?access_token=' . $access_token);
+		//Interpret data
+		$fbdata = json_decode($json_object);
+
+		foreach ($fbdata->data as $post) {
+			$posts .= '<p><a href="' . $post -> link . '">' . $post -> story . '</a></p>';
+			$posts .= '<p><a href="' . $post -> link . '">' . $post -> message . '</a></p>';
+			$posts .= '<p>' . $post -> description . '</p>';
+			$posts .= '<br />';
+		}
+		//Display the posts
+		echo $posts;
+		?>
 	</div>
 </div>
--- CodeIgniter-3.0.0/application/views/sidebar_view.php
+++ CodeIgniter-3.0.0/application/views/sidebar_view.php
@@ -22,7 +22,5 @@
 			<div class="fb-page" data-href="https://www.facebook.com/TEDxUHasselt?fref=ts" data-width="300" data-height="300" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/TEDxUHasselt?fref=ts"><a href="https://www.facebook.com/TEDxUHasselt?fref=ts">TEDxUHasselt</a></blockquote></div></div>
     	</div>
     </div>
-    
-    
-    
+   
 </div>
