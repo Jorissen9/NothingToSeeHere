8aa0f9992cffe485530bb237eef0207cb74eedeb
Kenny Vanrusselt <Kenny_Vanrusselt@hotmail.be>
2015-04-26 00:29:59 +0200

    mainpage content newsfeed

--- CodeIgniter-3.0.0/application/views/content_view.php
+++ CodeIgniter-3.0.0/application/views/content_view.php
@@ -3,21 +3,45 @@
 		<br/>
 		<h1>Welcome to TEDxUHasselt </h2>
 		<br/>
-		<h2> Nieuws </h1>
+		<h2> Latest News: </h1>
+		<hr>
 		<?php
-			$page_id = 'TEDxUHasselt';
-			$access_token = '384545391746432|kMtO9DQn412t0WTd6MfLo3fsKkI';
-			//Get the JSON
-			$json_object = @file_get_contents('https://graph.facebook.com/' . $page_id . '/posts?access_token=' . $access_token);
-			//Interpret data
-			$fbdata = json_decode($json_object);
+		$page_id = 'TEDxUHasselt';
+		$access_token = '384545391746432|kMtO9DQn412t0WTd6MfLo3fsKkI';
+		$fbid = "TEDxUHasselt";
+		$limit = 3;
+		$posts = "";
+		
+		//Get the JSON
+		$json_object = @file_get_contents('https://graph.facebook.com/' . $page_id . '/feed?limit=' . $limit . '&access_token=' . $access_token);
 
-			foreach ($fbdata->data as $post) {
-				$posts .= '<p><a href="' . $post -> link . '">' . $post -> story . '</a></p>';
+		//Interpret data
+		$fbdata = json_decode($json_object);
+
+		foreach ($fbdata->data as $post) {
+			        
+		 $fbTime = strtotime($post->created_time);
+         $myTime= date("d M Y h:ia",$fbTime);
+		 		
+			if(isset($post-> message))
+			{
+				$posts .= 'Posted on: ' . $myTime;
 				$posts .= '<p><a href="' . $post -> link . '">' . $post -> message . '</a></p>';
-				$posts .= '<p>' . $post -> description . '</p>';
-				$posts .= '<br />';
 			}
+
+			if(isset($post-> description))
+			{
+				$posts .= '<p><img src="' . $post -> picture . '" style="width:470px; height:246px;" /></p>';
+				$posts .= '<p>' . $post -> description . '</p>';
+				
+			}
+
+			
+			$posts .= '<br />';	
+			
+
+		}
+
 		//Display the posts
 		echo $posts;
 		?>
