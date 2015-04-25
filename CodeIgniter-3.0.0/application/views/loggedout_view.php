dfc0765466a9df6c23cd094a2868398fa41c3946
Jorissen9 <bjorn_jorissen@hotmail.com>
2015-04-25 21:38:31 +0200

    login button & bigger screens

--- CodeIgniter-3.0.0/application/views/loggedout_view.php
+++ CodeIgniter-3.0.0/application/views/loggedout_view.php
@@ -1,19 +1,19 @@
-<div class="col-sm-6 pull-right"> 	
-	
-    <div class="col-md-10 pull-right">
-        <div class="col-md-6 panel-body pull-right">
+<div class="col-sm-6 pull-right">
+
+	<div class="col-md-10 pull-right">
+		<div class="col-md-6 panel-body pull-right">
 			<div id="login" style="display: none">
-			<h1>Log in</h1>
-			<form>
-				<input type="email" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<div>
-					<input class="pull-right form-control btn" type="submit" value="Login" />
-				</div>
-			</form>
+				<h1>Log in</h1>
+				<form>
+					<input type="email" placeholder="Email" />
+					<input type="password" placeholder="Password" />
+					<div>
+						<input class="pull-right form-control btn" type="submit" value="Login" />
+					</div>
+				</form>
+			</div>
+			<a class="col-md-12 form-control btn" href="<?php echo site_url('login') ?>">Login</a>
+			<a class="col-md-12 form-control btn" href="">Register</a>
 		</div>
-		<a class="col-md-12 form-control btn" href="<?php echo site_url('login') ?>">Login</a>
-		<a class="col-md-12 form-control btn" href="">Register</a>
-		</div>
-    </div>
+	</div>
 </div>
