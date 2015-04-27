c518a6a06516e4ec97d196c52e997dcc63cdc7e2
Jorissen9 <bjorn_jorissen@hotmail.com>
2015-04-27 15:09:09 +0200

    Login & Register forms
    Ze zijn zichtbaar maar doen nog niets

--- CodeIgniter-3.0.0/application/views/header_view.php
+++ CodeIgniter-3.0.0/application/views/header_view.php
@@ -22,7 +22,26 @@
 
 		<script type="text/javascript" src="<?php echo base_url('assets/css/Navigation/assets/js/jquery.js')?>"></script>
 		<script type="text/javascript" src="<?php echo base_url('assets/js/Navigation/function.js')?>"></script>
-
+		<script type="text/javascript" >
+			function showLoginForm() {
+				showLoginButtons();
+				document.getElementById('login').style.display = 'block';
+				document.getElementById('loginShow').style.display = 'none';
+			}
+			function showRegisterForm() {
+				showLoginButtons();
+				document.getElementById('register').style.display = 'block';
+				document.getElementById('registerShow').style.display = 'none';
+				document.getElementById('loginSpace').style.display = 'none';
+			}
+			function showLoginButtons() {
+				document.getElementById('register').style.display = 'none';
+				document.getElementById('login').style.display = 'none';
+				document.getElementById('loginSpace').style.display = '';
+				document.getElementById('loginShow').style.display = 'block';
+				document.getElementById('registerShow').style.display = 'block';
+			}
+		</script>
 	</head>
 	<body></body>
 </html>
--- CodeIgniter-3.0.0/application/views/loggedout_view.php
+++ CodeIgniter-3.0.0/application/views/loggedout_view.php
@@ -4,17 +4,32 @@
 		<div class="col-md-6 panel-body pull-right">
 			<div id="login" style="display: none">
 				<h1>Log in</h1>
-				<form>
-					<input type="email" placeholder="Email" />
-					<input type="password" placeholder="Password" />
+				<form action="">
+					<input class="col-md-12 form-control" type="email" placeholder="Email" />
+					<div style="height: 5px" class="col-md-12"></div>
+					<input class="col-md-12 form-control" type="password" placeholder="Password" />
+					<div style="height: 5px" class="col-md-12"></div>
 					<div>
-						<input class="pull-right form-control btn" type="submit" value="Login" />
+						<input class="col-md-12 pull-right form-control btn" type="submit" value="Login" onclick="showLoginButtons()"/>
 					</div>
 				</form>
 			</div>
-			<a class="col-md-12 form-control btn" href="<?php echo site_url('login') ?>">Login</a>
-			<div class="col-md-12" style="height: 5px"></div>
-			<a class="col-md-12 form-control btn" href="">Register</a>
+			<div id="register" style="display: none">
+				<h1>Register</h1>
+				<form action="">
+					<input class="col-md-12 form-control" type="email" placeholder="Email" />
+					<div style="height: 5px" class="col-md-12"></div>
+					<input class="col-md-12 form-control" type="password" placeholder="Password" />
+					<div style="height: 5px" class="col-md-12"></div>
+					<div>
+						<input class="col-md-12 pull-right form-control btn" type="submit" value="Register" onclick="showLoginButtons()"/>
+					</div>
+					<div class="col-md-12" style="height: 5px"></div>
+				</form>
+			</div>
+			<a id="loginShow" class="col-md-12 form-control btn" onclick="showLoginForm()">Login</a>
+			<div id="loginSpace" class="col-md-12" style="height: 5px"></div>
+			<a id="registerShow" class="col-md-12 form-control btn" onclick="showRegisterForm()">Register</a>
 		</div>
 	</div>
 </div>
