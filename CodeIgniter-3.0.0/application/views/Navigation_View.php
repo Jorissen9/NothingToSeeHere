f027612c3baadcaff37593cb3899820e491006e7
Kenny Vanrusselt <Kenny_Vanrusselt@hotmail.be>
2015-04-29 01:26:06 +0200

    nav view update

--- CodeIgniter-3.0.0/application/views/Navigation_View.php
+++ CodeIgniter-3.0.0/application/views/Navigation_View.php
@@ -8,6 +8,9 @@
 					<a href="<?php echo base_url('home'); ?>" title="Home">Home</a>
 				</li>
 				<li class="col-lg-1">
+					<a href="#">Events</a>
+				</li>
+				<li class="col-lg-1">
 					<a href="#">Forums</a>
 				</li>
 				<li class="col-lg-1 parent">
@@ -32,10 +35,12 @@
 				</li>
 				<li class="col-lg-3	dropdown pull-right">
 					<a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
-					<div class="dropdown-menu" style="padding: 15px; padding-bottom: 15px;">
-						<form method="post" action="login" accept-charset="UTF-8">
-							<input class="form-control" style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
-							<input class="form-control" style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+					<div class="dropdown-menu" style="padding: 15px; padding-bottom: 15px; margin-top: 15px; width:300px;">
+						<?php echo validation_errors(); ?>
+   						<?php echo form_open('verifylogin', array('id'=>'frm')); ?>
+							<input class="form-control" style="margin-bottom: 15px;" type="text" value="<?php echo set_value('username'); ?>" placeholder="Username" id="username" name="username">
+							<input class="form-control" style="margin-bottom: 15px;" type="password" value="<?php echo set_value('password'); ?>" placeholder="Password" id="password" name="password">
+							<p id="error-message"></p>
 							<input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
 							<label class="string optional" for="user_remember_me"> Remember me</label>
 							<input class="btn btn-block form-control" type="submit" id="sign-in" value="Sign In">
