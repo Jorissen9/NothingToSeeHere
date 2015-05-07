91587b1c130d708d10eeef96e847d29e8d7f9745
Kenny Vanrusselt <Kenny_Vanrusselt@hotmail.be>
2015-05-07 23:47:13 +0200

    jsconnect integration part 1

--- application/models/user.php
+++ application/models/user.php
@@ -2,10 +2,10 @@
 Class User extends CI_Model {
 	
 	function login($username, $password) {
-		$this -> db -> select('id, username, password');
-		$this -> db -> from('users');
-		$this -> db -> where('username', $username);
-		$this -> db -> where('password', MD5($password));
+		$this -> db -> select('UserID, Name, Email, Photo');
+		$this -> db -> from('gdn_user');
+		$this -> db -> where('Email', $username);
+		$this -> db -> where('Passport', $password);
 		$this -> db -> limit(1);
 
 		$query = $this -> db -> get();
