a7c375ca66e1ced9bb74a9c3ab99eedbfdc57653
Kenny Vanrusselt <Kenny_Vanrusselt@hotmail.be>
2015-05-12 16:44:16 +0200

    forum height fix

--- application/views/signout_view.php
+++ application/views/signout_view.php
@@ -2,5 +2,5 @@
 	<a href="#" ><?php echo 'Hello, ' . $username ?></a>
 </li>
 <li class="col-lg-1 pull-right">
-	<a href="<?php echo base_url('forum/#/entry/signout?target=' . base_url('home'))?>" >Sign Out </a>
+	<a href="<?php echo base_url('forum/#/entry/signout/{Session_TransientKey}?Target=http%3A%2F%2Flocalhost/NothingToSeeHere/') ?>" >Sign Out </a>
 </li>
