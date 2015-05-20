6a41eeb7f30d2ce6999ddfd917bd48f7bbdb64a2
Jorissen9 <bjorn_jorissen@hotmail.com>
2015-05-20 18:23:47 +0200

    events layout

--- application/views/events_view.php
+++ application/views/events_view.php
@@ -1,35 +1,32 @@
-<?php
-var_dump($events);
+<div class="col-md-9">
+	<div  class="col-xs-10 pull-left col-xs-offset-1 content">
+		<?php
+		var_dump($events);
 
-
-foreach ($events as $row) {
+		foreach ($events as $row) {
 			$posts = "";
-			if (isset($row -> Picture))
-			{
-				$posts.= '<a href="#"><div class="col-xs-9 content" style=""> <div class="col-xs-9 col-xs-offset-3" style="background-color: #F5F5F5; background-image: url(./assets/imgs/' . $row -> Picture . '); text-align:center; border-radius: 25px;">';	
-			}
-			else
-			{
-				$posts.= '<a href="#"><div class="col-xs-9 content"><div class="col-xs-9 col-xs-offset-3" style="background-color: #F5F5F5; text-align:center; border-radius: 25px;">';	
+			if (isset($row -> Picture)) {
+				$posts .= '<a href="#"><div class="col-xs-12 content" style=""> <div class="col-xs-12" style="background-color: #F5F5F5; background-image: url(./assets/imgs/' . $row -> Picture . '); text-align:center; border-radius: 25px;">';
+			} else {
+				$posts .= '<a href="#"><div class="col-xs-12 content"><div class="col-xs-12" style="background-color: #F5F5F5; text-align:center; border-radius: 25px;">';
 			}
 
 			if (isset($row -> Title)) {
 				$posts .= '<a><h1>' . $row -> Title . '</h2></a><br />';
 			}
 			if (isset($row -> Date)) {
-				if (isset($row -> Time)){
-					$posts .= '<h5>When?</h5><h3>' . $row -> Date . ' at '. $row -> Time . '</h3><br />';
+				if (isset($row -> Time)) {
+					$posts .= '<h5>When?</h5><h3>' . $row -> Date . ' at ' . $row -> Time . '</h3><br />';
 				}
 			}
 			if (isset($row -> Description)) {
 				$posts .= '<h5>What?</h5><h3>' . $row -> Description . '</h3>';
 			}
- 			
-			
-			echo '<br/>	</div></a></div>';
-			
+
+			$posts .= '<br/>	</div></a></div>';
+
 			echo $posts;
 		}
-
-
-?>
+	?>
+	</div>
+</div>
